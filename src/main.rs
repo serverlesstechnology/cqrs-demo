1b5d33c2cb213d99de297a4962d748109cee15e1
Dave Garred <dgarred@starbucks.com>
2020-05-12 18:03:00 -0700

    add metadata

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![forbid(unsafe_code)]
 #![deny(clippy::all)]
 
+use std::collections::HashMap;
 use std::io::Read;
 
 use cqrs_es::{AggregateError, Command};
@@ -44,7 +45,13 @@ pub fn account_command(req: &mut Request) -> IronResult<Response> {
     match result {
         Ok(_) => Ok(Response::with(status::NoContent)),
         Err(err) => {
-            Ok(Response::with((status::BadRequest, err.to_string())))
+            let err_payload = match &err {
+                AggregateError::UserError(e) => serde_json::to_string(e).unwrap(),
+                AggregateError::TechnicalError(e) => e.clone(),
+            };
+            let mut response = Response::with((status::BadRequest, err_payload));
+            response.headers = std_headers();
+            Ok(response)
         }
     }
 }
@@ -53,13 +60,15 @@ fn process_command<T>(aggregate_id: &str, payload: String) -> Result<(), Aggrega
     where T: Command<BankAccount, BankAccountEvent> + DeserializeOwned
 {
     let payload = match serde_json::from_str::<T>(payload.as_str()) {
-        Ok(payload) => {payload},
+        Ok(payload) => { payload }
         Err(err) => {
-            return Err(AggregateError::TechnicalError(err.to_string()))
-        },
+            return Err(AggregateError::TechnicalError(err.to_string()));
+        }
     };
     let cqrs = cqrs_framework();
-    cqrs.execute(aggregate_id, payload)
+    let mut metadata = HashMap::new();
+    metadata.insert("time".to_string(), chrono::Utc::now().to_rfc3339());
+    cqrs.execute_with_metadata(aggregate_id, payload, metadata)
 }
 
 pub fn account_query(req: &mut Request) -> IronResult<Response> {
@@ -72,14 +81,18 @@ pub fn account_query(req: &mut Request) -> IronResult<Response> {
         }
         Some(query) => {
             let body = serde_json::to_string(&query).unwrap();
-            let mut headers = Headers::new();
-            let content_type = iron::headers::ContentType::json();
-            headers.set(content_type);
             let mut response = Response::with((status::Ok, body));
-            response.headers = headers;
+            response.headers = std_headers();
             Ok(response)
         }
     }
+}
+
+fn std_headers() -> Headers {
+    let mut headers = Headers::new();
+    let content_type = iron::headers::ContentType::json();
+    headers.set(content_type);
+    headers
 }
 
 type AccountQuery = GenericQueryRepository::<BankAccountQuery, BankAccount, BankAccountEvent>;
