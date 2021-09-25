5f6743a266856fb16aeaec36ea1b173c549a5d98
Dave Garred <dave.garred@serverlesstechnology.com>
2021-09-25 08:35:57 -0700

    Correct db columns to use 'json' where appropriate.

--- db/init.sql
+++ db/init.sql
@@ -3,8 +3,8 @@ CREATE TABLE events
     aggregate_type text                         NOT NULL,
     aggregate_id   text                         NOT NULL,
     sequence       bigint CHECK (sequence >= 0) NOT NULL,
-    payload        text                         NOT NULL,
-    metadata       text                         NOT NULL,
+    payload        json                         NOT NULL,
+    metadata       json                         NOT NULL,
     timestamp      timestamp with time zone DEFAULT (CURRENT_TIMESTAMP),
     PRIMARY KEY (aggregate_type, aggregate_id, sequence)
 );
@@ -13,7 +13,7 @@ CREATE TABLE account_query
 (
     query_instance_id text                        NOT NULL,
     version           bigint CHECK (version >= 0) NOT NULL,
-    payload           text                        NOT NULL,
+    payload           json                        NOT NULL,
     PRIMARY KEY (query_instance_id)
 );
 
